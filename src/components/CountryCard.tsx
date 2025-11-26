c8b23c0bae4a2a9ebe736677ad3146c83d57cd8d
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-11-26 07:05:00 -0800

    Add link to country card

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,18 +1,19 @@
 import type { CountryCard as CountryCardType } from "../services/countries";
+import { Link } from 'react-router-dom';
 
 export default function CountryCard({ card }: {card: CountryCardType}) {
+    const id = card.code ?? encodeURIComponent(card.name);
     
 
     return (
-        <div className="card">
+        <Link className="card" to={`/country/${id}`}>
             <img src={card.flagsSrc} alt={card.flagAlt} />
             <div className="card-body">
                 <h2 className="name">{card.name}</h2>
                 <p className="population">Population: {card.population ?? '-'}</p>
                 <p className="region">Region: {card.region ?? 'not found'}</p>
-                <p className="capital">Capital: {card.capital}</p>
+                <p className="capital">Capital: {card.capital ?? 'not found'}</p>
             </div>
-
-        </div>
+        </Link>
     )
 }
