6aa073884e0210445382d46a6822f11b9eaec946
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-11-26 00:16:43 -0800

    Create input and select

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,9 @@ export default function Home() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
 
+    const [search, setSearch] = useState('');
+    const [region, setRegion] = useState('');
+
     useEffect(() => {
         let mounted = true; 
         (async () => {
@@ -45,10 +48,26 @@ export default function Home() {
     }
 
     return (
-        <div>
-            {cards.slice(0, 5).map(card => (
-                <div key={card.code ?? card.name}></div>
-            ))}
-        </div>
+        <>
+            <section className="navbar">
+                <input 
+                type="search"
+                placeholder="Choose your country by name"
+                value={search}
+                onChange={event => setSearch(event.target.value)}
+                 />
+                 <select value={region} onChange={event => setRegion(event.target.value)}  name="" id="">
+                    <option value="">All Regions</option>
+                    <option value="Africa">Africa</option>
+                    <option value="Americas">Americas</option>
+                    <option value="Asia">Asia</option>
+                    <option value="Europe">Europe</option>
+                    <option value="Oceania">Oceania</option>
+                 </select>
+            </section>
+            <div>
+                Cards: {cards.length}
+            </div>
+        </>
     );
 }
